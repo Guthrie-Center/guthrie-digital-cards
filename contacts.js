143251f24898eb085a2aa88063cb2be41bcd9e46
Guthrie-Center <gc@springbranchisd.com>
2026-09-08 10:25:12 -0500

    Add contacts.js

---
+++ contacts.js
@@ -0,0 +1,426 @@
+const CONTACTS = [
+  {
+    "slug": "alan-yip-choy",
+    "name": "Alan Yip Choy",
+    "credentials": "",
+    "title": "3D Animation Instructor",
+    "phone": "713.251.1359",
+    "email": "Alan.YipChoy@springbranchisd.com"
+  },
+  {
+    "slug": "donna-benavidez",
+    "name": "Donna Benavidez",
+    "credentials": "",
+    "title": "Administrative Assistant",
+    "phone": "713.251.1311",
+    "email": "Donna.Benavidez@springbranchisd.com"
+  },
+  {
+    "slug": "halla-akshar",
+    "name": "Halla Akshar",
+    "credentials": "",
+    "title": "Administrative Assistant",
+    "phone": "713.251.1380",
+    "email": "Halla.Akshar@springbranchisd.com"
+  },
+  {
+    "slug": "mike-mcmanners",
+    "name": "Mike McManners",
+    "credentials": "M.Ed.",
+    "title": "Ag Center Enterprise Manager",
+    "phone": "713.251.1384",
+    "email": "Michael.Mcmanners@springbranchisd.com"
+  },
+  {
+    "slug": "katie-corona",
+    "name": "Katie Corona",
+    "credentials": "",
+    "title": "Agricultural Science | FFA Instructor",
+    "phone": "713.251.1381",
+    "email": "Kathryn.Corona@springbranchisd.com"
+  },
+  {
+    "slug": "madison-willis",
+    "name": "Madison Willis",
+    "credentials": "",
+    "title": "Agricultural Science | FFA Instructor",
+    "phone": "713.251.1385",
+    "email": "Madison.Willis@springbranchisd.com"
+  },
+  {
+    "slug": "nicole-nosrat",
+    "name": "Nicole Nosrat",
+    "credentials": "",
+    "title": "Agricultural Science | FFA Instructor",
+    "phone": "713.251.1383",
+    "email": "Nicole.Nosrat@springbranchisd.com"
+  },
+  {
+    "slug": "peyton-fisher",
+    "name": "Peyton Fisher",
+    "credentials": "",
+    "title": "Agricultural Science | FFA Instructor",
+    "phone": "713.251.1382",
+    "email": "Peyton.Fisher@springbranchisd.com"
+  },
+  {
+    "slug": "fuad-loutfi",
+    "name": "Fuad Loutfi",
+    "credentials": "",
+    "title": "Architectural Design Instructor",
+    "phone": "713.251.1356",
+    "email": "Fuad.Loutfi@springbranchisd.com"
+  },
+  {
+    "slug": "priya-natarajan",
+    "name": "Priya Natarajan",
+    "credentials": "AIA",
+    "title": "Architectural Design Instructor",
+    "phone": "713.251.1357",
+    "email": "Priya.Natarajan@springbranchisd.com"
+  },
+  {
+    "slug": "jane-primose",
+    "name": "Jane Primose",
+    "credentials": "M.Ed.",
+    "title": "Assistant Principal",
+    "phone": "713.251.1314",
+    "email": "Jane.Primrose@springbranchisd.com"
+  },
+  {
+    "slug": "rasha-altawash",
+    "name": "Rasha AlTawash",
+    "credentials": "",
+    "title": "Attendance Support",
+    "phone": "713.251.1301",
+    "email": "Rasha.AlTawash@springbranchisd.com"
+  },
+  {
+    "slug": "ezikiel-medrano",
+    "name": "Ezikiel Medrano",
+    "credentials": "",
+    "title": "Automotive Tech Instructor",
+    "phone": "713.251.1332",
+    "email": "Ezikiel.Medrano@springbranchisd.com"
+  },
+  {
+    "slug": "mike-barone",
+    "name": "Mike Barone",
+    "credentials": "",
+    "title": "Automotive Tech Instructor",
+    "phone": "713.251.1331",
+    "email": "Michael.Barone@springbranchisd.com"
+  },
+  {
+    "slug": "pedro-hernandez",
+    "name": "Pedro Hernandez",
+    "credentials": "",
+    "title": "Construction Tech Instructor",
+    "phone": "713.251.1335",
+    "email": "Pedro.Hernandez@springbranchisd.com"
+  },
+  {
+    "slug": "zubair-ulhaq",
+    "name": "Zubair Ulhaq",
+    "credentials": "",
+    "title": "Construction Tech Instructor",
+    "phone": "713.251.1333",
+    "email": "Zubair.Ulhaq@springbranchisd.com"
+  },
+  {
+    "slug": "emelin-salazar",
+    "name": "Emelin Salazar",
+    "credentials": "",
+    "title": "Cosmetology | Barbering Instructor",
+    "phone": "713.251.1350",
+    "email": "Emelin.Salazar@springbranchisd.com"
+  },
+  {
+    "slug": "grant-yip-choy",
+    "name": "Grant Yip Choy",
+    "credentials": "",
+    "title": "Cosmetology | Barbering Instructor",
+    "phone": "713.251.1351",
+    "email": "Grant.YipChoy@springbranchisd.com"
+  },
+  {
+    "slug": "yolanda-matthews-porter",
+    "name": "Yolanda Matthews-Porter",
+    "credentials": "",
+    "title": "Cosmetology | Barbering Instructor",
+    "phone": "713.251.1349",
+    "email": "Yolanda.MatthewsPorter@springbranchisd.com"
+  },
+  {
+    "slug": "anne-ivy",
+    "name": "Anne Ivy",
+    "credentials": "M.Ed.",
+    "title": "Counselor",
+    "phone": "713.251.1303",
+    "email": "Anne.Ivy@springbranchisd.com"
+  },
+  {
+    "slug": "dan-jones",
+    "name": "Dan Jones",
+    "credentials": "JD",
+    "title": "Criminal Justice Instructor",
+    "phone": "713.251.1346",
+    "email": "Daniel.Jones@springbranchisd.com"
+  },
+  {
+    "slug": "darrell-lewis",
+    "name": "Darrell Lewis",
+    "credentials": "JD",
+    "title": "Criminal Justice Instructor",
+    "phone": "713.251.1347",
+    "email": "Darrell.Lewis@springbranchisd.com"
+  },
+  {
+    "slug": "alex-bove",
+    "name": "Alex Bove",
+    "credentials": "",
+    "title": "Culinary Chef Instructor",
+    "phone": "713.251.1321",
+    "email": "Alexander.Bove@springbranchisd.com"
+  },
+  {
+    "slug": "andi-pope",
+    "name": "Andi Pope",
+    "credentials": "",
+    "title": "Culinary Chef Instructor",
+    "phone": "713.251.1327",
+    "email": "Andi.Pope@springbranchisd.com"
+  },
+  {
+    "slug": "ben-baze",
+    "name": "Ben Baze",
+    "credentials": "",
+    "title": "Culinary Chef Instructor",
+    "phone": "713.251.1320",
+    "email": "Benjamin.Baze@springbranchisd.com"
+  },
+  {
+    "slug": "sarah-mikhail",
+    "name": "Sarah Mikhail",
+    "credentials": "",
+    "title": "Culinary Chef Instructor",
+    "phone": "713.251.1319",
+    "email": "Sarah.Mikhail@springbranchisd.com"
+  },
+  {
+    "slug": "nakhestcia-franklin",
+    "name": "Nakhestcia Franklin",
+    "credentials": "",
+    "title": "Culinary Enterprise Manager",
+    "phone": "713.251.1328",
+    "email": "Nakhestcia.Franklin@springbranchisd.com"
+  },
+  {
+    "slug": "james-trower",
+    "name": "James Trower",
+    "credentials": "",
+    "title": "Cybersecurity Instructor",
+    "phone": "713.251.1362",
+    "email": "James.Trower@springbranchisd.com"
+  },
+  {
+    "slug": "jorge-mata",
+    "name": "Jorge Mata",
+    "credentials": "",
+    "title": "Electrician Tech Instructor",
+    "phone": "713.251.1339",
+    "email": "Jorge.MataTinajero@springbranchisd.com"
+  },
+  {
+    "slug": "martin-iniguez",
+    "name": "Martin Iniguez",
+    "credentials": "",
+    "title": "Electrician Tech Instructor",
+    "phone": "713.251.1341",
+    "email": "Martin.Iniguez@springbranchisd.com"
+  },
+  {
+    "slug": "horacio-rodr-guez",
+    "name": "Horacio Rodríguez",
+    "credentials": "",
+    "title": "Filmmaing | Audio Instructor",
+    "phone": "713.251.1345",
+    "email": "Horacio.Rodriguez@springbranchisd.com"
+  },
+  {
+    "slug": "carla-stevens",
+    "name": "Carla Stevens",
+    "credentials": "",
+    "title": "Front Office Support",
+    "phone": "713.251.1300",
+    "email": "Carla.Stevens@springbranchisd.com"
+  },
+  {
+    "slug": "arianna-sanchez",
+    "name": "Arianna Sanchez",
+    "credentials": "",
+    "title": "Health Science Instructor",
+    "phone": "713.251.1372",
+    "email": "Arianna.Sanchez@springbranchisd.com"
+  },
+  {
+    "slug": "kandi-caine",
+    "name": "Kandi Caine",
+    "credentials": "BSBA, CCMA",
+    "title": "Health Science Instructor",
+    "phone": "713.251.1366",
+    "email": "Kandice.Caine@springbranchisd.com"
+  },
+  {
+    "slug": "lj-garfield",
+    "name": "LJ Garfield",
+    "credentials": "",
+    "title": "Health Science Instructor",
+    "phone": "713.251.1367",
+    "email": "Lakeeta.Garfield@springbranchisd.com"
+  },
+  {
+    "slug": "jamy-fernandez",
+    "name": "Jamy Fernandez",
+    "credentials": "",
+    "title": "Health Science Instructor",
+    "phone": "713.251.1371",
+    "email": "Rut.FernandezSosa@springbranchisd.com"
+  },
+  {
+    "slug": "tatum-neely",
+    "name": "Tatum Neely",
+    "credentials": "",
+    "title": "Health Science Instructor",
+    "phone": "713.251.1363",
+    "email": "Tatum.Neely@springbranchisd.com"
+  },
+  {
+    "slug": "tracy-purvis",
+    "name": "Tracy Purvis",
+    "credentials": "M.A. Ed, BSBM, CPhT",
+    "title": "Health Science Instructor",
+    "phone": "713.251.1365",
+    "email": "Tracy.Purvis@springbranchisd.com"
+  },
+  {
+    "slug": "marlon-roquemore",
+    "name": "Marlon Roquemore",
+    "credentials": "",
+    "title": "Information Tech Instructor",
+    "phone": "713.251.1361",
+    "email": "Marlon.Roquemore@springbranchisd.com"
+  },
+  {
+    "slug": "tina-miller",
+    "name": "Tina Miller",
+    "credentials": "M.Ed.",
+    "title": "Internships Instructor",
+    "phone": "713.251.1358",
+    "email": "Tina.Miller@springbranchisd.com"
+  },
+  {
+    "slug": "hm1-dominic-monita",
+    "name": "HM1 Dominic Monita",
+    "credentials": "",
+    "title": "Naval Science Instructor",
+    "phone": "713.251.1344",
+    "email": "Dominic.Monita@springbranchisd.com"
+  },
+  {
+    "slug": "ana-lopez",
+    "name": "Ana Lopez",
+    "credentials": "",
+    "title": "Operations",
+    "phone": "346.741.3345",
+    "email": "Anavesi.LopezDeVides@springbranchisd.com"
+  },
+  {
+    "slug": "carmen-flores",
+    "name": "Carmen Flores",
+    "credentials": "",
+    "title": "Operations",
+    "phone": "832.461.7624",
+    "email": "Maria.Flores2@springbranchisd.com"
+  },
+  {
+    "slug": "miriam-abeja",
+    "name": "Miriam Abeja",
+    "credentials": "",
+    "title": "Operations",
+    "phone": "832.994.2549",
+    "email": "Miriam.Abeja@springbranchisd.com"
+  },
+  {
+    "slug": "tristan-moreno",
+    "name": "Tristan Moreno",
+    "credentials": "",
+    "title": "Operations",
+    "phone": "346.465.6645",
+    "email": "Tristan.Moreno@springbranchisd.com"
+  },
+  {
+    "slug": "al-trevino",
+    "name": "Al Trevino",
+    "credentials": "",
+    "title": "Operations Foreman",
+    "phone": "713.514.2906",
+    "email": "Alfredo.Trevino@springbranchisd.com"
+  },
+  {
+    "slug": "joe-kolenda",
+    "name": "Joe Kolenda",
+    "credentials": "",
+    "title": "Principal",
+    "phone": "713.251.1310",
+    "email": "Joe.Kolenda@springbranchisd.com"
+  },
+  {
+    "slug": "brandie-argueta",
+    "name": "Brandie Argueta",
+    "credentials": "RN BSN",
+    "title": "Registered Nurse",
+    "phone": "713.251.1317",
+    "email": "Brandie.Argueta@springbranchisd.com"
+  },
+  {
+    "slug": "ryan-hardgraves",
+    "name": "Ryan Hardgraves",
+    "credentials": "DPC",
+    "title": "Salon Enterprise Manager",
+    "phone": "713.251.1352",
+    "email": "Ryan.Hardgraves@springbranchisd.com"
+  },
+  {
+    "slug": "cdr-bill-breland",
+    "name": "CDR Bill Breland",
+    "credentials": "",
+    "title": "Senior Naval Science Instructor",
+    "phone": "713.251.1343",
+    "email": "William.Breland@springbranchisd.com"
+  },
+  {
+    "slug": "allie-adams",
+    "name": "Allie Adams",
+    "credentials": "M.Ed.",
+    "title": "Special Education | 504 Liaison",
+    "phone": "713.251.1364",
+    "email": "Allie.Adams@springbranchisd.com"
+  },
+  {
+    "slug": "erick-rodriguez",
+    "name": "Erick Rodriguez",
+    "credentials": "",
+    "title": "Welding Instructor",
+    "phone": "713.251.1338",
+    "email": "Erick.Rodriguez@springbranchisd.com"
+  },
+  {
+    "slug": "mike-martinez",
+    "name": "Mike Martinez",
+    "credentials": "",
+    "title": "Welding Instructor",
+    "phone": "713.251.1336",
+    "email": "Michael.Martinez@springbranchisd.com"
+  }
+];
